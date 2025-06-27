a3724ed1b4f07bb205d59c0573361f818964fc5e
jihyung <aiu.614.dev@gmail.com>
2025-06-27 16:53:05 +0900

    feat: Implement user login functionality

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hellom Git World from IntelliJ!");
         System.out.println("Local Change: Adding a new feature.");
+        System.out.println("Login feature: User authentication Logic.");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
