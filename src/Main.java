379f64c9aed29887b5c139a91dac937872ca7918
jihyung <aiu.614.dev@gmail.com>
2025-06-27 16:57:38 +0900

    refactor: UI updates on develop branch

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.printf("Hellom Git World from IntelliJ!");
         System.out.println("Local Change: Adding a new feature.");
         System.out.println("Login feature: User authentication Logic.");
+        System.out.println("New line from develop branch for UI update.");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
