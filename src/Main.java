3886a868cadecc3efa744605d841693fffde060f
jihyung <aiu.614.dev@gmail.com>
2025-06-27 16:56:27 +0900

    feat: Implement user logout functionality (local)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.printf("Hellom Git World from IntelliJ!");
         System.out.println("Local Change: Adding a new feature.");
         System.out.println("Login feature: User authentication Logic.");
+        System.out.println("Logout feature: User logoug process.");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
